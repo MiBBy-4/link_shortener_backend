5d8de66f0bc9bb85efc428ef949272cb6df881ca
MiBBy-4 <maksim.762373maks@bk.ru>
2022-04-05 19:55:25 +0300

    feat: add development database connection

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,8 @@
 # Load the Rails application.
-require_relative "application"
+require_relative 'application'
+
+database_connection_file = File.join(Rails.root, 'database_connection.env')
+load(database_connection_file) if File.exist?(database_connection_file)
 
 # Initialize the Rails application.
 Rails.application.initialize!
