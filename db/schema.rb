067344751082edf1b3d28d57488f82b818d01b04
MiBBy-4 <maksim.762373maks@bk.ru>
2022-04-11 14:58:06 +0300

    feat: add User model with validation and migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_05_170145) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_11_114658) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,6 +18,14 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_05_170145) do
     t.string "base_link"
     t.string "shorted_link"
     t.integer "views", default: 0
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
+    t.integer "role"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
