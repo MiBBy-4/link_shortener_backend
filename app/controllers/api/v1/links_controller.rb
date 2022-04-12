4f699711463089df02207030f838b05b9345c88c
MiBBy-4 <maksim.762373maks@bk.ru>
2022-04-12 21:08:43 +0300

    feat: add description parameter in permit, also change range of random str size in LinksController

--- app/controllers/api/v1/links_controller.rb
+++ app/controllers/api/v1/links_controller.rb
@@ -45,7 +45,7 @@ class Api::V1::LinksController < ApplicationController
 
   def generate_shorted_link
     charset = Array('A'..'Z') + Array('a'..'z')
-    Array.new(rand(4..25)) { charset.sample }.join
+    Array.new(rand(4..15)) { charset.sample }.join
   end
 
   def set_link
@@ -53,6 +53,6 @@ class Api::V1::LinksController < ApplicationController
   end
 
   def link_params
-    params.require(:link).permit(:base_link)
+    params.require(:link).permit(:base_link, :description)
   end
 end
