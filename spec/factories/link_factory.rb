80e16010b93e3d474d4af923892aa8e95c04e81f
MiBBy-4 <maksim.762373maks@bk.ru>
2022-04-16 22:31:15 +0300

    fix: add description field in link factory

--- spec/factories/link_factory.rb
+++ spec/factories/link_factory.rb
@@ -2,6 +2,7 @@ FactoryBot.define do
   factory :link do
     base_link { Faker::Internet.url }
     shorted_link { Faker::String.random(length: 3..15) }
+    description { Faker::Lorem.paragraph }
     user {User.last}
   end
 end
